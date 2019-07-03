6747e2b2415eac3baee996f40d44d72e22a8adb1
abidex4yemi <abidex4yemi@gmail.com>
2019-07-03 13:19:42 +0100

    Create router method for get single user

--- router/index.js
+++ router/index.js
@@ -12,13 +12,48 @@ const home = (request, response) => {
 	}
 };
 
+// Return all lambda students list
+const serveStudents = (request, response) => {
+	const dataRequestType = request.url.substr(request.url.length - 5);
+	if (dataRequestType === '.json') {
+		const username = request.url.replace('/', '').replace('.json', '');
+
+		// List of all users
+		const users = [
+			{
+				name: 'Yemi',
+				schoolName: 'Lambda',
+				graduatingDate: '31/09/2019',
+				stack: 'Javascript, Node, Python'
+			},
+			{
+				name: 'Tola',
+				schoolName: 'Lambda',
+				graduatingDate: '31/09/2019',
+				stack: 'Javascript, Node, Python'
+			},
+			{
+				name: 'Jane',
+				schoolName: 'Lambda',
+				graduatingDate: '31/09/2019',
+				stack: 'Javascript, Node, Python'
+			}
+		];
+
+		// find matching user
+		const user = users.filter(user => user.name.toLowerCase() === username.toLowerCase());
+		console.log(user);
+	}
+};
+
 // Handle stylesheet (css) incoming request
 const css = (request, response) => {
 	if (request.url.indexOf('.css') !== -1 && request.url.indexOf('.css')) {
 		const file = fs.readFileSync(`.${request.url}`, 'UTF-8');
+		response.writeHead(200, { 'Content-Type': 'text/css' });
 		response.write(file);
 		response.end();
 	}
 };
 
-module.exports = { home, css };
+module.exports = { home, css, serveStudents };
